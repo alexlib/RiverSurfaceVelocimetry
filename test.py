17aefd05d1d1dbff85586a3f48a877c3da152f8b
wyxgoishin <wyxgoishin@qq.com>
2021-03-05 15:18:03 +0800

    change

--- test.py
+++ test.py
@@ -1,19 +1,20 @@
-import cv2
-from skimage.transform import radon
-import numpy as np
+class Solution:
+    def dailyTemperatures(self, ops):
+        op, index = [], -1
+        for ch in ops:
+            if len(ch) > 1 or ('0' <= ch <= '9'):
+                op.append(int(ch))
+                index += 1
+            elif ch == 'C':
+                del(op[index])
+                index -= 1
+            elif ch == 'D':
+                op.append(op[index] * 2)
+                index += 1
+            elif ch == '+':
+                op.append(op[index] + op[index-1])
+                index += 1
+        return sum(op)
 
-
-fai = 31.6
-image_path = 'sti_imgs/fai=%s.png' % (str(fai))
-img = cv2.imread(image_path)
-img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-
-theta = np.linspace(-90., 90., max(img.shape), endpoint=False)
-sinogram = radon(img, theta=theta, circle=True)
-sinogram = cv2.normalize(sinogram, None, 0, 255, cv2.NORM_MINMAX, dtype=cv2.CV_8U)
-cv2.imshow('radon', sinogram)
-cv2.waitKey(0)
-
-
-
-
+ops = ["1","C","-62","-45","-68"]
+print(Solution().dailyTemperatures(ops))
